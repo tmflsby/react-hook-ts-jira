9db76f5d3c2b7d78e621ac22fea84a628ec91d27
liuyang <823246840@qq.com>
2021-12-13 15:19:54 +0800

    feat: 添加工程列表

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,43 @@
+import * as qs from "qs";
+import {useEffect, useState} from "react";
+import {SearchPanel} from "./search-panel";
+import {List} from "./list";
+import {cleanObject} from "../../utils";
+
+const apiUrl = process.env.REACT_APP_API_URL
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: '',
+    personId: ''
+  })
+  const [list, setList] = useState([])
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+      if (response.ok) {
+        setList(await response.json())
+      }
+    })
+  }, [param])
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async response => {
+      if (response.ok) {
+        setUsers(await response.json())
+      }
+    })
+  }, [])
+
+  return <div>
+    <SearchPanel
+      param={param}
+      setParam={setParam}
+      users={users}
+    />
+    <List
+      list={list}
+      users={users}
+    />
+  </div>
+}
